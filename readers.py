87978ca05da05321031d57f95a791bae701f393b
Ben Davies <bens.public@gmail.com>
2025-05-28 11:54:49 +1000

    Add xarray GRIB reader.

--- readers.py
+++ readers.py
@@ -4,6 +4,7 @@ TODO: readers for opening GRIB files with different libraries
 
 from osgeo import gdal
 import pygrib
+import xarray as xr
 
 gdal.UseExceptions()
 
@@ -29,3 +30,17 @@ def read_data_gdal(path, nband):
     ds = None  # close dataset  # noqa
 
     return data
+
+
+def read_data_xarray(path, nband):
+    # TODO: does xarray use GDAL???
+    # checked in virtual env, raw xarray from pip does NOT have a GDAL dependency
+
+    ds = xr.open_dataset(path, engine="cfgrib")
+
+    assert ds.r is not None
+    if ds.r.dims == ('latitude', 'longitude'):
+        # there are no time or pressure dimensions, ignore nband & return data
+        return ds.r.data
+
+    raise NotImplementedError("Handle time/pressure bands")

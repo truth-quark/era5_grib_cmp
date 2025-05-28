087e528a6c5cec758997b2259a083444712a78fd
Ben Davies <bens.public@gmail.com>
2025-05-28 15:10:16 +1000

    Add test to confirm multi variable read order for different libraries.

--- test_subset_era5.py
+++ test_subset_era5.py
@@ -1,6 +1,5 @@
 import readers as rd
 
-import numpy as np
 import pytest
 
 
@@ -18,9 +17,15 @@ def aus_1_band_subset_path():
     # This is contrived but limits the number of variables in reader testing
     return "data/20230201_reanalysis_rh_level_1hpa_midnight_aus_subset.grib"
 
+
 @pytest.fixture
 def aus_2_band_subset_path():
     return "data/20230201_reanalysis_rh_level_1hpa_2hpa_midnight_aus_subset.grib"
+
+
+@pytest.fixture
+def aus_6_band_subset_path():
+    return "data/20230201_reanalysis_rh_3_pressure_levels_2_timestamps_aus_subset.grib"
 
 
 def test_gdal_pygrib_single_band_read_matches(aus_1_band_subset_path):
@@ -44,6 +49,8 @@ def test_gdal_pygrib_single_band_read_matches(aus_1_band_subset_path):
 # FIXME test a subset file with 1 timestamp & 2 pressure levels
 def test_multi_band_read_matches(aus_2_band_subset_path):
     for band in (0, 1):
+
+        # TODO: inefficient due to multiple file reads
         gdal_data = rd.read_data_gdal(aus_2_band_subset_path, band)
         pygrib_data = rd.read_data_pygrib(aus_2_band_subset_path, band)
 
@@ -61,7 +68,44 @@ def test_multi_band_read_matches(aus_2_band_subset_path):
 
 
 # FIXME test a subset file with 2 timestamps & 1 pressure level
-# FIXME test a subset file with 2 timestamps & 2 pressure levels
+# FIXME test a subset file with 2 timestamps & 3 pressure levels
+def test_6_band_data_read(aus_6_band_subset_path):
+    # gdal order from gdalinfo is:
+    # timestep 0, pressure 1
+    # timestep 0, pressure 2
+    # timestep 0, pressure 3
+    # timestep 1, pressure 1
+    # timestep 1, pressure 2
+    # timestep 1, pressure 3
+
+    n_timestamps = 2
+    n_pressure_levels = 3
+
+    x_bands = {0: (0,0),
+               1: (0,1),
+               2: (0,2),
+               3: (1,0),
+               4: (1,1),
+               5: (1,2),
+               }
+
+    # FIXME: find neater way for xarray "band" number
+    for band in range(n_timestamps * n_pressure_levels):
+
+        # TODO: inefficient due to multiple file reads
+        gdal_data = rd.read_data_gdal(aus_6_band_subset_path, band)
+        pygrib_data = rd.read_data_pygrib(aus_6_band_subset_path, band)
+        xarray_data = rd.read_data_xarray(aus_6_band_subset_path, x_bands[band])
+
+        matches = gdal_data == pygrib_data
+        assert matches.all(), f"GDAL & pygrib data reads are different for band {band}"
+        assert ((0.0 <= gdal_data) <= 100.0).all(), f"Invalid GDAL RH data found for band {band}"
+
+        # second stage match
+        matches2 = gdal_data == xarray_data
+        assert matches2.all(), f"GDAL & xarray data reads are different for band {band}"
+        assert ((0.0 <= xarray_data) <= 100.0).all(), f"Invalid xarray RH data found for band {band}"
+
 
 
 # nlat, nlon = gdal_data.shape

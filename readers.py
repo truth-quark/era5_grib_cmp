8ba578503dafdd60733274195ddbe474dabbe2d4
Ben Davies <bens.public@gmail.com>
2025-05-28 10:38:00 +1000

    Add GDAL & pygrib reader functions.

---
+++ readers.py
@@ -0,0 +1,29 @@
+"""
+TODO: readers for opening GRIB files with different libraries
+"""
+
+from osgeo import gdal
+import pygrib
+
+
+# TODO: add xarray GRIB reader
+
+def read_data_pygrib(path, nband):
+    # TODO: is band zero or one index?
+    # TODO: add handling for large(r) data, such as global
+    ds = pygrib.open(path)
+    ds.seek(nband)
+    record = ds.read(1)[0]  # extract from a list
+    data = record.values
+    ds.close()
+
+    return data
+
+
+def read_gdal_data(path, nband):
+    ds = gdal.Open(path)
+    band = ds.GetRasterBand(nband+1)
+    data = band.ReadAsArray()  # TODO: warning performance for large arrays
+    ds = None  # close dataset  # noqa
+
+    return data

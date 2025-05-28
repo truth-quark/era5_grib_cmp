3ec4e7bacdeb879439558e43548de9dd45218f50
Ben Davies <bens.public@gmail.com>
2025-05-28 12:57:38 +1000

    Add test to compare xarray data "band" order.

--- test_subset_era5.py
+++ test_subset_era5.py
@@ -18,6 +18,10 @@ def aus_1_band_subset_path():
     # This is contrived but limits the number of variables in reader testing
     return "data/20230201_reanalysis_rh_level_1hpa_midnight_aus_subset.grib"
 
+@pytest.fixture
+def aus_2_band_subset_path():
+    return "data/20230201_reanalysis_rh_level_1hpa_2hpa_midnight_aus_subset.grib"
+
 
 def test_gdal_pygrib_single_band_read_matches(aus_1_band_subset_path):
     band = 0  # using 0 for 1st band
@@ -35,6 +39,29 @@ def test_gdal_pygrib_single_band_read_matches(aus_1_band_subset_path):
     matches2 = gdal_data == xarray_data
     assert matches2.all(), "GDAL & xarray data reads are different"
     assert ((0.0 <= xarray_data) <= 100.0).all(), "Invalid xarray RH data found"
+
+
+# FIXME test a subset file with 1 timestamp & 2 pressure levels
+def test_multi_band_read_matches(aus_2_band_subset_path):
+    for band in (0, 1):
+        gdal_data = rd.read_data_gdal(aus_2_band_subset_path, band)
+        pygrib_data = rd.read_data_pygrib(aus_2_band_subset_path, band)
+
+        xband = -(band + 1)  # NB xarray bands appear to be reverse order!
+        xarray_data = rd.read_data_xarray(aus_2_band_subset_path, xband)
+
+        matches = gdal_data == pygrib_data
+        assert matches.all(), f"GDAL & pygrib data reads are different for band {band}"
+        assert ((0.0 <= gdal_data) <= 100.0).all(), f"Invalid GDAL RH data found for band {band}"
+
+        # second stage match
+        matches2 = gdal_data == xarray_data
+        assert matches2.all(), f"GDAL & xarray data reads are different for band {band}"
+        assert ((0.0 <= xarray_data) <= 100.0).all(), f"Invalid xarray RH data found for band {band}"
+
+
+# FIXME test a subset file with 2 timestamps & 1 pressure level
+# FIXME test a subset file with 2 timestamps & 2 pressure levels
 
 
 # nlat, nlon = gdal_data.shape
